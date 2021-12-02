b2b7188a91dfb034ba75d8eaf71e117a8f9cb180
Daniel Lu <daniel_lu@hotmail.com>
2021-12-01 22:00:19 -0500

    pair programming done

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -20,6 +20,22 @@ describe("#wordSearch()", function() {
     assert.isFalse(result);
   });
 
+  it("should return false if the word is not present", function() {
+    const result = wordSearch([
+      ['F', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['R', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['A', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['N', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['K', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['L', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['I', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['N', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANKLINE')
+
+    assert.isTrue(result);
+  });
+
   it("should return true if the word is present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-module.exports = wordSearch
+  let switchLetters = letters[0].map((col, c) => letters.map((row, r) => letters[r][c]));
+
+  const horizontalJoin2 = switchLetters.map(ls => ls.join(''));
+ 
+  for (let l of horizontalJoin2) {
+    if (l.includes(word)) return true;
+  }
+  return false;
+};
+
+module.exports = wordSearch;
